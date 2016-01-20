ba74aafb736ac376e1ac6632bd12db91233dc75f
username <newhuidong@newhuidongdeMac-mini.local>
2016-01-20 17:35:43 +0800

    no message

---
+++ CustumPopView/CustumPopView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustumPopView
+//
+//  Created by newhuidong on 16/1/20.
+//  Copyright © 2016年 HuiDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustumPopView/CustumPopView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustumPopView
+//
+//  Created by newhuidong on 16/1/20.
+//  Copyright © 2016年 HuiDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
